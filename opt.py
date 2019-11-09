61c33f6d95e31251a7baec5ba924b29f5580b910
fyscu <arukidasuzhang@gmail.com>
2019-11-10 02:05:26 +0800

    init3

--- opt.py
+++ opt.py
@@ -2,8 +2,15 @@ learning_rate = 1e-3
 batch_size = 256
 num_workers = 5
 seq_max_lens = 24
+epoch = 1000
+num_classes = 313
+drop_rate = 0.2
+init_features_num = 64
+growth_rate = 32
+BN_size = 4
 train_image_file = "data/train/img"
 train_label_file = "data/labels.txt"
 test_image_file = "data/test/img"
 dictionary = "dictionary/dictionary.csv"
-epoch = 1000
+load_model = ''
+
